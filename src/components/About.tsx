afbe34b59cecdc67635ff857b4323b7a654353dc
PinchaoRamiro <antonio.pinchao@utp.edu.co>
2024-07-28 18:56:33 -0500

    Add some changes into visual and information

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,14 +4,7 @@ const About: React.FC = () => (
   <section id="about">
     <h2>Sobre Mí</h2>
     <p>
-    Como desarrollador web full stack, mi objetivo es aplicar mis conocimientos en tecnologías frontend y backend,
-     junto con mi experiencia en control de versiones, bases de datos, multiples tecnologias de bakend y frontend,
-     leguajes de programación y metodologías ágiles,
-     para crear aplicaciones web robustas y escalables. Aspiro a contribuir en equipos colaborativos utilizando 
-     principios de programación orientada a objetos y buenas prácticas de desarrollo, mientras sigo ampliando
-     mis habilidades en redes, optimización SEO, y DevOps, todas las tecnologias que se apliquen a cierta area de trabajo.
-    Estoy comprometido con el aprendizaje continuo y la adopción 
-     de nuevas tecnologías para ofrecer soluciones innovadoras y eficientes.
+    Como desarrollador web full stack, mi objetivo es aplicar mis conocimientos en tecnologías frontend y backend, junto con mi experiencia en control de versiones, bases de datos, múltiples tecnologías de bakend y frontend, leguajes de programación y metodologías ágiles para crear aplicaciones web robustas y escalables. Aspiro a contribuir en equipos colaborativos utilizando principios de programación orientados a objetos y buenas prácticas de desarrollo, mientras continuó ampliando mis habilidades en redes, optimización SEO, DevOps y todas las tecnologías que se aplican en una determinada área de trabajo. Estoy comprometido con el aprendizaje continuo y la adaptación a nuevas tecnologías para ofrecer soluciones innovadoras y eficientes.
     </p>
   </section>
 );
