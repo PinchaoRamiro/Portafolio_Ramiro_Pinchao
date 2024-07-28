f2d219e2082087b26b296b8a11fa90ed2912056c
PinchaoRamiro <antonio.pinchao@utp.edu.co>
2024-07-28 18:45:38 -0500

    Add some changes into visual and information

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact: React.FC = () => (
   <section id="contact">
     <h2>Contacto</h2>
     <p>Email: rmrpinchao@gmail.com</p>
-    <p>Numero de celular: (+57) 3217849789</p>
+    <p>Número de celular: (+57) 3217849789</p>
     <p className='a-contact'>Github: <a href="https://github.com/PinchaoRamiro">https://github.com/PinchaoRamiro</a></p>
     <p className='a-contact'>Linkedin: <a href="https://www.linkedin.com/in/ramiro-antonio-pinchao-chachinoy-5038a931b">https://www.linkedin.com/in/ramiro-antonio-pinchao-chachinoy</a></p>
   </section>
